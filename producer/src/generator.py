c97f1d6cb38972d93569b3012d68bef97f726465
Stanley Hsu <stanh002@gmail.com>
2026-07-06 17:25:30 +0800

    feat(generator): added logging for generator

--- producer/src/generator.py
+++ producer/src/generator.py
@@ -1,12 +1,20 @@
 import time
+import logging
+
 from src.transaction_factory import create_transaction
 from src.settings import TRANSACTIONS_PER_SECOND
+from src.logging_config import setup_logging
+
+logger = logging.getLogger(__name__)
 
 def main():
+    setup_logging()
+
     sequence_number = 1
+
     while True:
         transaction = create_transaction(sequence_number)
-        print(transaction.model_dump_json(indent=2))
+        logger.info(transaction.model_dump_json())
         sequence_number += 1
         time.sleep(1 / TRANSACTIONS_PER_SECOND)
 
