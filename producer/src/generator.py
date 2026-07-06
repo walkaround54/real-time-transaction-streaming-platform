45499d03e74fad07e43f366ba505dcd6309354d5
Stanley Hsu <stanh002@gmail.com>
2026-07-06 17:45:53 +0800

    feat(generator): added unit tests for transaction factory and generator

--- producer/src/generator.py
+++ producer/src/generator.py
@@ -7,14 +7,18 @@ from src.logging_config import setup_logging
 
 logger = logging.getLogger(__name__)
 
+def generate_one_transaction(sequence_number: int):
+    transaction = create_transaction(sequence_number)
+    logger.info("Generated transaction %s", transaction.model_dump_json())
+    return transaction
+
 def main():
     setup_logging()
 
     sequence_number = 1
 
     while True:
-        transaction = create_transaction(sequence_number)
-        logger.info(transaction.model_dump_json())
+        generate_one_transaction(sequence_number)
         sequence_number += 1
         time.sleep(1 / TRANSACTIONS_PER_SECOND)
 
---
+++ producer/tests/test_generator.py
@@ -0,0 +1,9 @@
+from src.generator import generate_one_transaction
+
+def test_generate_one_transaction():
+    transaction = generate_one_transaction(1)
+
+    assert transaction.transaction_id == "TX00000001"
+    assert transaction.customer_id.startswith("CUST")
+    assert transaction.account_id.startswith("ACC")
+    assert transaction.amount > 0
