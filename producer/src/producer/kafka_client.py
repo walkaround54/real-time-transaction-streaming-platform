72eb47dd4700e18036ff84af0977bf4a734075f8
Stanley Hsu <stanh002@gmail.com>
2026-07-15 16:59:22 +0800

    feat(kafka_client.py): added buffering, batch size configuration and delivery callback for producer client

--- producer/src/producer/kafka_client.py
+++ producer/src/producer/kafka_client.py
@@ -7,13 +7,25 @@ class KafkaPublisher:
             "bootstrap.servers": bootstrap_servers,
             "acks": "all",
             "enable.idempotence": True, # more useful for when I replicate cluster
+
+            "batch.num.messages": 10, # for Java Producer, user batch.size
+            "linger.ms": 5,
         })
+
+    @staticmethod
+    def delivery_report(err, msg):
+        if err is not None:
+            print(
+                f"Delivery failed for topic={msg.topic()} "
+                f"partition={msg.partition()} offset={msg.offset()}: {err}"
+            )
 
     def publish(self, value:str, key: str | None = None) -> None:
         self.producer.produce(
             topic = self.topic,
             value = value,
             key = key,
+            callback = self.delivery_report
         )
         self.producer.poll(0)
 
