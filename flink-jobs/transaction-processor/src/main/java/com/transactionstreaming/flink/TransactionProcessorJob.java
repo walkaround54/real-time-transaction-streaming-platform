cb2d4c351fbf0997d3ccc09f420b538346870817
Stanley Hsu <stanh002@gmail.com>
2026-07-22 16:57:23 +0800

    feat: added kafka topic for transactions_processed

--- flink-jobs/transaction-processor/src/main/java/com/transactionstreaming/flink/TransactionProcessorJob.java
+++ flink-jobs/transaction-processor/src/main/java/com/transactionstreaming/flink/TransactionProcessorJob.java
@@ -1,6 +1,10 @@
 package com.transactionstreaming.flink;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 
 public class TransactionProcessorJob {
     public static void main(String[] args) throws Exception {
@@ -8,9 +12,17 @@ public class TransactionProcessorJob {
 
         env.setParallelism(1);
 
-        env.fromData("Flink transaction processor scaffold working").print();
+        KafkaSource<String> source = KafkaSource.<String>builder()
+            .setBootstrapServers("localhost:9092")
+            .setTopics("transactions_raw")
+            .setGroupId("flink-transactions-processor")
+            .setStartingOffsets(OffsetsInitializer.earliest())
+            .setValueOnlyDeserializer(new SimpleStringSchema())
+            .build();
 
-        env.execute("transaction-processor");
-
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "transactions-raw-source")
+            .print();
+        
+            env.execute("transaction-processor");
     }
 }
